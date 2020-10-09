6ae0e76b66770b0d7513398ceef7456552b75e07
Juan Carlos Pleitez <jcpleitez@outlook.com>
2020-10-09 16:31:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,10 +7,8 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  total = 0 
   array.each_with_index do |index, character|
   index += 1
   puts "#{index}. #{character}"
 end
-total += 1
 end
